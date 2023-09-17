a8ccdd68eeb629bdd0a6bb530cc77b5ca0f0cf1a
Justin <web@jstn.ly>
2023-09-18 01:02:07 +0700

    updates to use Apollo Explorer, an updated playground

--- src/gql-config.service.ts
+++ src/gql-config.service.ts
@@ -3,11 +3,31 @@ import { ConfigService } from '@nestjs/config';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory } from '@nestjs/graphql';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
+import { ApolloServerPlugin, BaseContext } from '@apollo/server';
 
 @Injectable()
 export class GqlConfigService implements GqlOptionsFactory {
   constructor(private configService: ConfigService) {}
+
   createGqlOptions(): ApolloDriverConfig {
+    let env = this.configService.get<string>('NODE_ENV');
+    let plugins: ApolloServerPlugin<BaseContext>[];
+
+    if (env === 'production') {
+      plugins = [
+        ApolloServerPluginLandingPageProductionDefault({
+          embed: true,
+          graphRef: 'myGraph@prod',
+        }),
+      ];
+    } else {
+      plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+    }
+
     const graphqlConfig = this.configService.get<GraphqlConfig>('graphql');
     return {
       // schema options
@@ -19,8 +39,10 @@ export class GqlConfigService implements GqlOptionsFactory {
       // subscription
       installSubscriptionHandlers: true,
       includeStacktraceInErrorResponses: graphqlConfig.debug,
-      playground: graphqlConfig.playgroundEnabled,
+      playground: false,
+      introspection: graphqlConfig.playgroundEnabled,
       context: ({ req }) => ({ req }),
+      plugins,
     };
   }
 }
