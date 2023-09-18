1e89edfd67bd4391e7da60b9b9cc52654ce083ff
Justin <web@jstn.ly>
2023-09-18 23:54:20 +0700

    removing Post from project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppResolver } from './app.resolver';
-import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -34,7 +33,6 @@ import config from './common/configs/config';
 
     AuthModule,
     UsersModule,
-    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
